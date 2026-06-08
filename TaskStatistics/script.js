5cf1e3bb75484dacda76157178a71869f4c0d2aa
Sanya9700 <sanya25051991@gmail.com>
2026-06-08 09:07:25 +0400

    search

--- TaskStatistics/script.js
+++ TaskStatistics/script.js
@@ -4,19 +4,23 @@ const uncompletedTask = document.querySelector(".uncompleted");
 const addTask = document.querySelector(".add-task");
 const addBtn = document.querySelector(".addbtn");
 const searchTask = document.querySelector(".search-task");
+const sortBtn = document.querySelector(".sort");
 
 const tasks = [
   {
+    id: Date.now(),
     title: "Сделать домашку",
     completed: true,
     important: true,
   },
   {
+    id: Date.now(),
     title: "Помыть посуду",
     completed: false,
     important: false,
   },
   {
+    id: Date.now(),
     title: "Подготовиться к контрольной по математике",
     completed: false,
     important: true,
@@ -27,8 +31,12 @@ function renderTask() {
   for (let task of tasks) {
     const cardTask = document.createElement("div");
     const title = document.createElement("p");
+    const btnDel = document.createElement("button");
     title.innerHTML = task.title;
     cardTask.append(title);
+    cardTask.append(btnDel);
+    btnDel.innerHTML = "Удалить";
+    btnDel.dataset.id = task.id
     tasksResulat.append(cardTask);
   }
 }
@@ -72,15 +80,35 @@ function getLongestTask() {
 }
 // console.log(getLongestTask());
 
-addBtn.addEventListener('click', () => {
-  tasks.push({title: addTask.value})
-  tasksResulat.innerHTML = ''
-  renderTask()
-})
+addBtn.addEventListener("click", () => {
+  tasks.push({ title: addTask.value });
+  tasksResulat.innerHTML = "";
+  renderTask();
+});
 
-searchTask.addEventListener('input', () => {
-    function findTask(title){
-      return tasks.find(tasks => tasks.title === title)
-    }
+searchTask.addEventListener("input", () => {
+  function findTask(title) {
+    return tasks.find((tasks) => tasks.title === title);
+  }
   console.log(findTask(searchTask.value));
-})
+});
+
+function sortTasks() {
+  tasks.sort((a, b) => b.important - a.important);
+}
+
+sortBtn.addEventListener("click", () => {
+  sortTasks();
+  tasksResulat.innerHTML = "";
+  renderTask();
+  console.log(tasks);
+});
+
+function delTask() {
+  const delTaskBtn = document.querySelectorAll(".btnDel");
+  for (let delBtn of delTaskBtn) {
+    console.log(delBtn);
+  }
+  console.log(delTaskBtn);
+}
+delTask();
